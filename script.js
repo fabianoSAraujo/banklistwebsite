655379bd5c59a8dfa4badc32e09ad54c9f8f70dc
fabianoSAraujo <araujo.s.fabiano@gmail.com>
2021-03-26 14:14:58 +0000

    Implementacao sticky menu

--- script.js
+++ script.js
@@ -99,5 +99,26 @@ const handleHover = (e, opacity) => {
     logo.style.opacity = opacity;
   }
 };
+//Passing "argument" into handler
+
 nav.addEventListener('mouseover', e => handleHover(e, 0.5));
 nav.addEventListener('mouseout', e => handleHover(e, 1));
+
+// Sticky navigation
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = entries => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
